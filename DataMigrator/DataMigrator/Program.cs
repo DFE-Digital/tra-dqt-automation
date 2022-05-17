65c1180a3206e3ce01ba90f4a0d55c70206a6bc3
Kevin Joy <mrkevinjoy@outlook.com>
2022-05-17 11:05:38 +0100

    sample console application for updating new fields after husid has been split

---
+++ DataMigrator/DataMigrator/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddTransient<DataService>();
+            }).Build();
+        
+        var dataservice = host.Services.GetRequiredService<DataService>();
+        var command = args.Length > 0 ? args[0] : String.Empty;
+        await dataservice.ExecuteAsync(command);
+    }
+}
+
