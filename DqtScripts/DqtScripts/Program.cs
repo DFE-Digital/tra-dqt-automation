37df497e186410ad9fa9a3320301fdf88b739bd8
James Gunn <james@gunn.dev>
2022-05-19 15:15:36 +0100

    Add DqtScripts app and workflow (#29)
    * sample console application for updating new fields after husid has been split
    
    * sample console application for updating new fields after husid has been split
    
    * add gitignore file
    
    * add gitignore file
    
    * version number bump
    
    * Move .gitignore up a level and remove ignored files
    
    * Rename DataMigrator to DqtScripts
    
    * Remove appsettings.json
    
    * Add .editorconfig file
    
    * Move to System.CommandLine
    
    * Add workflow for running script
    
    Co-authored-by: Kevin Joy <mrkevinjoy@outlook.com>

---
+++ DqtScripts/DqtScripts/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.PowerPlatform.Dataverse.Client;
+
+await CreateCommandLineBuilder()
+    .UseHost((IHostBuilder host) => host
+        .ConfigureAppConfiguration(config => config
+            .AddUserSecrets(typeof(Program).Assembly)
+            .AddEnvironmentVariables())
+        .ConfigureServices(services =>
+        {
+            services.AddSingleton<ServiceClient>(sp =>
+            {
+                var configuration = sp.GetRequiredService<IConfiguration>();
+
+                return new ServiceClient(
+                    new Uri(configuration["CrmUrl"]),
+                    configuration["CrmClientId"],
+                    configuration["CrmClientSecret"],
+                    useUniqueInstance: false);
+            });
+        }))
+    .UseDefaults()
+    .Build()
+    .InvokeAsync(args);
+
+static CommandLineBuilder CreateCommandLineBuilder()
+{
+    var rootCommand = CreateRootCommand();
+    return new CommandLineBuilder(rootCommand);
+}
+
+static RootCommand CreateRootCommand()
+{
+    var rootCommand = new RootCommand(description: "DQT scripts");
+
+    AddWhoAmICommand(rootCommand);
+
+    return rootCommand;
+}
+
+static void AddWhoAmICommand(RootCommand rootCommand)
+{
+    var command = new Command("whoami", description: "Prints the user ID")
+    {
+        Handler = CommandHandler.Create<IHost>(async host =>
+        {
+            var serviceClient = host.Services.GetRequiredService<ServiceClient>();
+
+            var response = (WhoAmIResponse)await serviceClient.ExecuteAsync(new WhoAmIRequest());
+            Console.WriteLine(response.UserId);
+        })
+    };
+
+    rootCommand.Add(command);
+}
---
+++ DqtScripts/DqtScripts/README.txt
@@ -0,0 +1,28 @@
+﻿# Description
+This console application is intended to be used with a git workflow to run adhoc to run data migrations against a CRM instance
+
+# Usage:
+
+dotnet run DataMigrator.dll {command}
+
+e.g.
+dotnet run DataMigrator.dll husid
+
+or from github workflows tab:
+
+enter command name that will be passed in as an input argument
+
+Each command requires a corresponding piece of code to handle it's action.
+
+# Adding new columns in CRM
+
+- Login to Build CRM which is used as a seed environent for others. (https://ent-dqt-build.crm4.dynamics.com/)
+- Click Components 
+- Select Entity that requires new fields
+- Select fields
+- Click new button
+- Fill in new field name details (name, length etc...)
+- Click save and close
+- Bump version number found under information -> Version field
+- Export solution
+- Check exported solution into source control
