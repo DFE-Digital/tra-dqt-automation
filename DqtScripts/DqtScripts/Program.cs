1c4929333b9142c4f87c94a63dbb55d0f8bb147a
James Gunn <james@gunn.io>
2022-05-19 14:49:29 +0100

    Move to System.CommandLine

--- DqtScripts/DqtScripts/Program.cs
+++ DqtScripts/DqtScripts/Program.cs
@@ -1,20 +1,63 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.PowerPlatform.Dataverse.Client;
 
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        var host = Host.CreateDefaultBuilder()
-            .ConfigureServices((hostContext, services) =>
+await CreateCommandLineBuilder()
+    .UseHost((IHostBuilder host) => host
+        .ConfigureAppConfiguration(config => config
+            .AddUserSecrets(typeof(Program).Assembly)
+            .AddEnvironmentVariables())
+        .ConfigureServices(services =>
+        {
+            services.AddSingleton<ServiceClient>(sp =>
             {
-                services.AddTransient<DataService>();
-            }).Build();
-        
-        var dataservice = host.Services.GetRequiredService<DataService>();
-        var command = args.Length > 0 ? args[0] : String.Empty;
-        await dataservice.ExecuteAsync(command);
-    }
+                var configuration = sp.GetRequiredService<IConfiguration>();
+
+                return new ServiceClient(
+                    new Uri(configuration["CrmUrl"]),
+                    configuration["CrmClientId"],
+                    configuration["CrmClientSecret"],
+                    useUniqueInstance: false);
+            });
+        }))
+    .UseDefaults()
+    .Build()
+    .InvokeAsync(args);
+
+static CommandLineBuilder CreateCommandLineBuilder()
+{
+    var rootCommand = CreateRootCommand();
+    return new CommandLineBuilder(rootCommand);
 }
 
+static RootCommand CreateRootCommand()
+{
+    var rootCommand = new RootCommand(description: "DQT scripts");
+
+    AddWhoAmICommand(rootCommand);
+
+    return rootCommand;
+}
+
+static void AddWhoAmICommand(RootCommand rootCommand)
+{
+    var command = new Command("whoami", description: "Prints the user ID")
+    {
+        Handler = CommandHandler.Create<IHost>(async host =>
+        {
+            var serviceClient = host.Services.GetRequiredService<ServiceClient>();
+
+            var response = (WhoAmIResponse)await serviceClient.ExecuteAsync(new WhoAmIRequest());
+            Console.WriteLine(response.UserId);
+        })
+    };
+
+    rootCommand.Add(command);
+}
