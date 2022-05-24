6965572f067bce8722ee14e1131c8faad00d75a3
James Gunn <james@gunn.io>
2022-05-24 13:44:06 +0100

    Add command for backing up dfeta_husid values

--- DqtScripts/DqtScripts/Program.cs
+++ DqtScripts/DqtScripts/Program.cs
@@ -4,11 +4,14 @@ using System.CommandLine.Hosting;
 using System.CommandLine.NamingConventionBinder;
 using System.CommandLine.Parsing;
 using Azure.Storage.Blobs;
+using CsvHelper;
 using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 
 await CreateCommandLineBuilder()
     .UseHost((IHostBuilder host) => host
@@ -51,13 +54,14 @@ static RootCommand CreateRootCommand()
     var rootCommand = new RootCommand(description: "DQT scripts");
 
     AddWhoAmICommand(rootCommand);
+    AddBackupHusidsCommand(rootCommand);
 
     return rootCommand;
 }
 
 static void AddWhoAmICommand(RootCommand rootCommand)
 {
-    var command = new Command("whoami", description: "Prints the user ID")
+    var command = new Command("whoami", description: "Prints the user ID.")
     {
         Handler = CommandHandler.Create<IHost>(async host =>
         {
@@ -65,6 +69,64 @@ static void AddWhoAmICommand(RootCommand rootCommand)
 
             var response = (WhoAmIResponse)await serviceClient.ExecuteAsync(new WhoAmIRequest());
             Console.WriteLine(response.UserId);
+        })
+    };
+
+    rootCommand.Add(command);
+}
+
+static void AddBackupHusidsCommand(RootCommand rootCommand)
+{
+    var command = new Command("backup-husids", description: "Creates a CSV backup of contacts with a dfeta_husid.")
+    {
+        Handler = CommandHandler.Create<IHost>(async host =>
+        {
+            var serviceClient = host.Services.GetRequiredService<ServiceClient>();
+            var blobContainerClient = host.Services.GetRequiredService<BlobContainerClient>();
+
+            await blobContainerClient.CreateIfNotExistsAsync();
+
+            var backupBlobName = $"backup-husids/backup_{DateTime.Now:yyyyMMddHHmmss}.csv";
+            var backupBlobClient = blobContainerClient.GetBlobClient(backupBlobName);
+
+            // https://github.com/Azure/azure-sdk-for-net/pull/28148 - we have to pass options
+            using (var blobStream = await backupBlobClient.OpenWriteAsync(overwrite: true, new Azure.Storage.Blobs.Models.BlobOpenWriteOptions()))
+            using (var streamWriter = new StreamWriter(blobStream))
+            using (var csvWriter = new CsvWriter(streamWriter, System.Globalization.CultureInfo.CurrentCulture))
+            {
+                csvWriter.WriteField("contactid");
+                csvWriter.WriteField("dfeta_husid");
+                csvWriter.WriteField("dfeta_trn");
+                csvWriter.NextRecord();
+
+                var query = new QueryExpression("contact");
+                query.Criteria.AddCondition("dfeta_husid", ConditionOperator.NotNull);
+                query.ColumnSet = new ColumnSet("contactid", "dfeta_husid", "dfeta_trn");
+                query.PageInfo = new PagingInfo()
+                {
+                    Count = 1000,
+                    PageNumber = 1
+                };
+
+                EntityCollection result;
+
+                do
+                {
+                    result = await serviceClient.RetrieveMultipleAsync(query);
+
+                    foreach (var record in result.Entities)
+                    {
+                        csvWriter.WriteField(record.GetAttributeValue<Guid>("contactid"));
+                        csvWriter.WriteField(record.GetAttributeValue<string>("dfeta_husid"));
+                        csvWriter.WriteField(record.GetAttributeValue<string>("dfeta_trn"));
+                        csvWriter.NextRecord();
+                    }
+
+                    query.PageInfo.PageNumber++;
+                    query.PageInfo.PagingCookie = result.PagingCookie;
+                }
+                while (result.MoreRecords);
+            }
         })
     };
 
